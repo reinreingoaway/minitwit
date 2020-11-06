dd5f8f50b5c2b1bc465d9cf55b3f44ea20b1f27d
Ballon Marc Reinhard <DL6151@engie.com>
2020-11-06 15:42:34 +0800

    add serverless p requirements

--- backend/service/messages.py
+++ backend/service/messages.py
@@ -12,37 +12,36 @@ class Message(Model):
 
         message_id = UnicodeAttribute(hash_key=True)
         date = UTCDateTimeAttribute(range_key=True)
-        name = UnicodeAttribute(null=False)
-        content = UnicodeAttribute(null=False)
+        name = UnicodeAttribute()
+        content = UnicodeAttribute()
 
 
 def get_message(event, context):
-    event_body = json.loads(event)["body"]
+    print("event_body", event["body"])
+    event_body = json.loads(event["body"])
     response = {}
 
-    try:
-        messages = Message.query(limit=5, last_evaluated_key=event_body["lastEvaluatedKey"])
-        message_serializable = []
-        for message in messages:
-            message_serializable.append({
-                "date": message.date,
-                "name": message.name,
-                "content": message.content,
-            })
-        
+    # try:
+    messages = Message.query("999")#limit=5, last_evaluated_key=event_body["lastEvaluatedKey"])
+    message_serializable = []
+    print("messages", messages)
+    for message in messages:
+        message_serializable.append({
+            "date": message.date,
+            "name": message.name,
+            "content": message.content,
+        })
 
-        response = {
-            "statusCode": 200,
-            "body": json.dumps({ "lastEvaluatedKey": messages.last_evaluated_key, "messages": message_serializable })
-        }
-    except:
-        response = {
-            "statusCode": 500,
-        }
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({ "messages": message_serializable })
+    }
+    # except:
+    #     response = {
+    #         "statusCode": 500,
+    #     }
 
     return response
-
-# get_message(json.dumps({ "body": { "lastEvaluatedKey": "aa"}}), {})
 
 def create_message(event, context):
     event_body = json.loads(event["body"])
